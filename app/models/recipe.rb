b6379ba1d8902ee98cdda863a22ff3b7b58bb6d3
leonaso <leoar_soto@yahoo.com>
2016-12-22 18:06:30 +0000

    Add category view

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,7 +2,7 @@ class Recipe < ApplicationRecord
   belongs_to :user
   has_many :recipe_categories
   has_many :categories, through: :recipe_categories
-  validates :title, presence: true, length: {maximum: 50} 
-  validates :description, presence: true, length: {maximum: 200}
+  validates :title, presence: true, length: {minimum: 5, maximum: 50} 
+  validates :description, presence: true, length: {minimum: 5, maximum: 200}
   validates :user_id, presence: true
 end
