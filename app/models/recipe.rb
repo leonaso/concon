9671cb92ee40c47a2fb88fd5aa4a3e53c98d531f
leonaso <leoar_soto@yahoo.com>
2016-12-22 14:46:56 +0000

    Add recipe and category association

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,7 @@
 class Recipe < ApplicationRecord
   belongs_to :user
+  has_many :recipe_categories
+  has_many :categories, through: :recipe_categories
   validates :title, presence: true, length: {maximum: 50} 
   validates :description, presence: true, length: {maximum: 200}
   validates :user_id, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161220234324) do
+ActiveRecord::Schema.define(version: 20161222021154) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "recipe_categories", force: :cascade do |t|
+    t.integer  "recipe_id"
+    t.integer  "category_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "recipes", force: :cascade do |t|
