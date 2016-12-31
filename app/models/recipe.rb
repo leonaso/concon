4ba6ec7c64a7bf9a690cfc0ac769474d13509c60
leonaso <leoar_soto@yahoo.com>
2016-12-31 21:50:49 +0000

    change minimun length recipe description

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,7 +2,7 @@ class Recipe < ApplicationRecord
   belongs_to :user
   has_many :recipe_categories
   has_many :categories, through: :recipe_categories
-  validates :title, presence: true, length: {minimum: 5, maximum: 50} 
-  validates :description, presence: true, length: {minimum: 5, maximum: 200}
+  validates :title, presence: true, length: {minimum: 3, maximum: 50} 
+  validates :description, presence: true, length: {minimum: 3, maximum: 1024}
   validates :user_id, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :recipes, dependent: :destroy
   before_save { self.email = email.downcase }
   validates :username, presence: true, uniqueness: {case_sensitive: false}, 
-                      length: {minimum: 3, maximum: 50 }
+                      length: {minimum: 2, maximum: 50 }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :email, presence: true, length: { maximum: 255 },
                     uniqueness: { case_sensitive: false },
